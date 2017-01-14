ed953352ea8ea84e3d7ec8d58c0fe26d8f72ae4a
nhtb <l5309070@std.yildiz.edu.tr>
2017-01-15 01:09:46 +0300

    ready

--- src/app/services/in.module.ts
+++ src/app/services/in.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CShellComponent } from '../clientshell/clientshell.component';
 import { childRouter } from '../../assets/router';
+
 import { ClientComponent } from '../clientshell/client/client.component';
-import { ClientdetailComponent } from '../clientdetail/clientdetail.component';
+import { ClientdetailComponent } from '../clientshell/clientdetail/clientdetail.component';
+import { CShellComponent } from '../clientshell/clientshell.component';
 
 @NgModule({
   declarations: [
--- src/assets/router.ts
+++ src/assets/router.ts
@@ -2,10 +2,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './../app/login/login.component';
 import { HomeComponent } from '../app/home/home.component';
-import { IsUserLoggedIn } from '../app/services/user/user.service';
 import { SignComponent } from '../app/sign/sign.component';
+
+import { IsUserLoggedIn } from '../app/services/user/user.service';
+
 import { CShellComponent } from '../app/clientshell/clientshell.component';
 import { ClientComponent } from '../app/clientshell/client/client.component';
+import { ClientdetailComponent } from '../app/clientshell/clientdetail/clientdetail.component';
 
 const rootRoutes: Routes = [
     { path: 'login', component: LoginComponent },
@@ -20,7 +23,8 @@ export const rootRouter = RouterModule.forRoot(rootRoutes);
 const childRoutes: Routes = [
     {
         path: 'client', children: [
-            { path: '', component: ClientComponent }
+            { path: '', component: ClientComponent },
+            { path: ':id', component: ClientdetailComponent}
         ]
     }
 ];
