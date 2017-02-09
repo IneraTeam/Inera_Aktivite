f79783935c531535a09fa7a33c0997d163bbacf2
btoparlak <l5309070@std.yildiz.edu.tr>
2017-02-09 19:14:55 +0300

    login yapısı değiştirildi

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth/auth.service';
 import { UserService } from '../services/user/user.service';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -16,7 +17,25 @@ export class LoginComponent implements OnInit {
   }
 
   submitForm(value) {
-    this.user.login(value);
+    this.user.login(value)
+      .then(() => {
+        this.user.basics.then(basics => {
+          this.user.setLocalInfo(basics['name'], basics['role'])
+            .then(() => this.user.navInChild());
+        });
+      })
+      .catch((err) => {
+        // non user || pre user
+        this.user.checkIfPreUser(value.email).take(1).subscribe(data => {
+          if (data.length !== 0) {
+            // pre user
+            this.user.navParentRoute('/sign/', { queryParams: { mail: value.email, key: data[0].$key } });
+          } else {
+            // non user
+            console.log('Error@LoginComponent.ts :', err);
+          }
+        });
+      });
   }
 
   redirectSignIn() {
