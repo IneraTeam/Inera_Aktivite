17547f3dc988f431acab8ee988d2119f7d635309
gizemneboglu <gizem.neboglu@inera.com.tr>
2017-02-16 14:01:35 +0300

    pul

--- src/app/actshell/actshell.component.ts
+++ src/app/actshell/actshell.component.ts
@@ -2,17 +2,18 @@ import { Component, OnInit } from '@angular/core';
 export class Activies{
   abbrv: string;
   addres: string;
-  createdAt:string;
+  createdAt:number;
   name: string;
   p_mail: string;
   p_name:string;
 }
 
 const ActList: Activies[]= [
-  {abbrv:"TTF",addres:"Inera", createdAt:"10.02.2017",name:"Batu", p_mail:"batu@batu.com", p_name:"Muhatap"},
-  {abbrv:"TTF",addres:"Inera", createdAt:"10.02.2017",name:"Batu", p_mail:"batu@batu.com", p_name:"Muhatap"},
-  {abbrv:"TTF",addres:"Inera", createdAt:"10.02.2017",name:"Batu", p_mail:"batu@batu.com", p_name:"Muhatap"},
-  {abbrv:"TTF",addres:"Inera", createdAt:"10.02.2017",name:"Batu", p_mail:"batu@batu.com", p_name:"Muhatap"}
+  {abbrv:"TTF",addres:"Inera", createdAt: Date.now(),name:"Batu", p_mail:"batu@batu.com", p_name:"Muhatap"},
+  {abbrv:"TTF",addres:"Inera", createdAt: Date.now(),name:"Batu", p_mail:"batu@batu.com", p_name:"Muhatap"},
+  {abbrv:"TTF",addres:"Inera", createdAt: Date.now(),name:"Batu", p_mail:"batu@batu.com", p_name:"Muhatap"},
+  {abbrv:"TTF",addres:"Inera", createdAt: Date.now(),name:"Batu", p_mail:"batu@batu.com", p_name:"Muhatap"}
+ 
 ]
 @Component({
   selector: 'app-actshell',
@@ -29,6 +30,9 @@ export class ActshellComponent implements OnInit {
 
   addActivity(){
 
+  }
+  navActDetail(){
+    
   }
 
 }
